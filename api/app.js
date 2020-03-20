83e7983724d71c4fb6582659b9ab974f2e0d6b39
Arnaud Birk <arnaud.birk@ign.fr>
2020-03-20 17:19:59 +0100

    fix doc

--- api/app.js
+++ api/app.js
@@ -1,29 +1,33 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
 const port = 3000
 
 const db = require('./queries')
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+const app = express()
 
-//app.get('/', (request, response) => {
-//  response.json({ info: 'Node.js, Express, and Postgres API' })
-//})
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+	console.log("----------------------------")
+	console.log(req.method, ' ', req.path, ' ', JSON.stringify(req.query))
+        console.log("received at " + Date.now())
+	next();
+})
+
+app.use('/api/vendor/swagger-editor', express.static(__dirname + '/node_modules/swagger-editor-dist'));
+app.use('/api/vendor/swagger-ui', express.static(__dirname + '/node_modules/swagger-ui-dist'));
+app.use('/api/doc', express.static(__dirname + '/../doc'));
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
 
 app.get('/jobs', db.getFirstJobReady)
 app.post('/jobs', db.insertJob)
 app.put('/jobs/:id', db.updateJob)
 
-app.use('/api/vendor/swagger-editor', express.static(__dirname + 'node_modules/swagger-editor-dist'));
-app.use('/api/vendor/swagger-ui', express.static(__dirname + 'node_modules/swagger-ui-dist'));
-app.use('/api/doc', express.static(__dirname + '/../doc'));
-
 app.listen(port, function () {
-  console.log('App running on port ${port}.')
+  console.log('App running on port '+port)
 })
--- launch.sh
+++
@@ -1,1 +0,0 @@
-supervisor api/app.js
