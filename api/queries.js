ce91874f79362582914eaabf47b0f966a7095045
Arnaud Birk <arnaud.birk@gmail.com>
2020-03-20 13:16:05 +0100

    Ajout api.env pour gérer les connectîons à postgres

--- api/queries.js
+++ api/queries.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'gpao',
-  password: 'postgres',
-  port: 5432,
+        user: process.env.PGUSER,
+        host: process.env.PGHOST,
+        database: process.env.PGDATABASE,
+        password: process.env.PGPASSWORD,
+        port: process.env.PGPORT
 })
 
  const getFirstJobReady = (request, response) => {
